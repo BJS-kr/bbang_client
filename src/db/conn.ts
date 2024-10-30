9c955f74924efac66dd64aa76929b70ec2e86997
BJS-kr <hahajeng1234@gmail.com>
2024-10-30 13:48:59 +0900

    chore: remove unused file

--- src/db/conn.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/mysql2';
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not defined as an environment variable');
-}
-
-const db = drizzle(process.env.DATABASE_URL);
