08be758f23d80c2bbcca8eb1fe469f346ba6e085
BJS-kr <hahajeng1234@gmail.com>
2024-10-31 12:30:44 +0900

    fix: add roomId when creating room

--- src/utils/writePayload.ts
+++ src/utils/writePayload.ts
@@ -1,4 +1,4 @@
-import { log } from '../utils/logger';
+import { error, log } from '../utils/logger';
 import { createPacket, encodePayload } from '../protobuf/packet';
 
 export function writePayload(socket, packetType, version, sequence, payload) {
@@ -9,6 +9,6 @@ export function writePayload(socket, packetType, version, sequence, payload) {
 
     socket.write(packet);
   } catch (e) {
-    console.error(e);
+    error(e);
   }
 }
