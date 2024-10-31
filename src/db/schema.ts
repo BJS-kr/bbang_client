0215ec8ffbe2d2db28873dde34a5933444a144d6
BJS-kr <hahajeng1234@gmail.com>
2024-10-31 10:27:29 +0900

    feat: credentials update

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,7 @@ export const $users = mysqlTable('users', {
   id: varchar('id', { length: 36 }).primaryKey(),
   userId: varchar('user_id', { length: 15 }).notNull().unique(),
   password: varchar('password', { length: 30 }).notNull(),
-  nickname: varchar('nickname', { length: 15 }).default('').notNull().unique(),
+  nickname: varchar('nickname', { length: 15 }).unique(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
