10f70b83cf3c0358ac6173ce5b08c0cdb4b18555
BJS-kr <hahajeng1234@gmail.com>
2024-10-31 10:45:09 +0900

    fix: nickname to nullable

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,7 @@ export const $users = mysqlTable('users', {
   id: varchar('id', { length: 36 }).primaryKey(),
   userId: varchar('user_id', { length: 15 }).notNull().unique(),
   password: varchar('password', { length: 30 }).notNull(),
-  nickname: varchar('nickname', { length: 15 }).notNull().default(''),
+  nickname: varchar('nickname', { length: 15 }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
