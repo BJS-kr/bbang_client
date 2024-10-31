9cad5d29d97f4e10fd64b5ec2e9c8971484b1824
BJS-kr <hahajeng1234@gmail.com>
2024-10-31 12:16:37 +0900

    feat: leave room when disconnected

--- src/events/onEnd.ts
+++ src/events/onEnd.ts
@@ -2,9 +2,11 @@ import net from 'node:net';
 import { Context } from './types';
 import { session } from '../users/session';
 import { log } from '../utils/logger';
+import { rooms } from '../rooms/rooms';
 
 export const onEnd = (socket: net.Socket, ctx: Context) => () => {
   log('클라이언트와의 연결이 종료되었습니다.');
   session.quit(ctx.userId);
+  rooms.quit(ctx.roomId, ctx.userId, ctx);
   socket.end();
 };
--- src/events/onError.ts
+++ src/events/onError.ts
@@ -1,9 +1,11 @@
 import net from 'node:net';
 import { Context } from './types';
 import { session } from '../users/session';
+import { rooms } from '../rooms/rooms';
 
 export const onError = (socket: net.Socket, ctx: Context) => (error) => {
   console.error('Socket error:', error);
   session.quit(ctx.userId);
+  rooms.quit(ctx.roomId, ctx.userId, ctx);
   socket.end();
 };
