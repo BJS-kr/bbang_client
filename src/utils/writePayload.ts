34e19e4ba9ce80d2e1d902f6ebf2be4de1b2fe9a
yj han <passioncitizen@gmail.com>
2024-11-01 13:57:41 +0900

    chore: add decodeTest

--- src/utils/writePayload.ts
+++ src/utils/writePayload.ts
@@ -1,10 +1,13 @@
 import { error, log } from '../utils/logger';
-import { createPacket, encodePayload } from '../protobuf/packet';
+import { createPacket, decodePayload, encodePayload } from '../protobuf/packet';
 
 export function writePayload(socket, packetType, version, sequence, payload) {
   try {
     log(`writePayload: ${JSON.stringify(payload, (key, value) => (key === 'socket' ? undefined : value))}`);
     const encodedPayload = encodePayload(packetType, payload);
+
+    // TODO 테스트
+    log(`decodePayload: ${JSON.stringify(decodePayload(packetType, encodedPayload), (key, value) => (key === 'socket' ? undefined : value))}`);
     const packet = createPacket(packetType, version, sequence, encodedPayload);
 
     socket.write(packet);
