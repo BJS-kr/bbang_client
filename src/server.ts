d84d15792a8c7ecf48b0420dc5035b95a219fb3c
BJS-kr <hahajeng1234@gmail.com>
2024-11-06 12:47:41 +0900

    chore: recheck

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,15 @@ import { config } from './config/config';
 import { onConnection } from './events/onConnection';
 import { error, log } from './utils/logger';
 
-// process.on('uncaughtException', (err) => {
-//   error('Uncaught Exception:', err);
-//   process.exit(1);
-// });
+process.on('uncaughtException', (err) => {
+  error('Uncaught Exception:', err);
+  process.exit(1);
+});
 
-// process.on('unhandledRejection', (reason, promise) => {
-//   error('Unhandled Rejection at:', promise, 'reason:', reason);
-//   process.exit(1);
-// });
+process.on('unhandledRejection', (reason, promise) => {
+  error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
 
 const server = net.createServer(onConnection);
 
