d5baf288afedc41de2a8064cd45e5a2d9b4dd9cc
passioncitizen <passioncitzen@gmail.com>
2024-11-10 22:46:26 +0900

    fix: fix bugs.

--- src/game/win.condition.ts
+++ src/game/win.condition.ts
@@ -16,35 +16,33 @@ function isAllDeadExceptPsychopath(room: Room) {
   return room.users.filter((u) => u.character.roleType !== RoleType.PSYCHOPATH).every((u) => u.character.isDead());
 }
 export function checkWinCondition(rooms: Rooms, room: Room, roomId: number) {
-  return () => {
-    if (isHitMenAllDead(room)) {
-      room.broadcast(PACKET_TYPE.GAME_END_NOTIFICATION, {
-        winners: room.users.filter((u) => u.character.roleType === RoleType.TARGET || u.character.roleType === RoleType.BODYGUARD).map((u) => u.id),
-        winType: WinType.TARGET_AND_BODYGUARD_WIN,
-      } satisfies MessageProps<S2CGameEndNotification>);
+  if (isHitMenAllDead(room)) {
+    room.broadcast(PACKET_TYPE.GAME_END_NOTIFICATION, {
+      winners: room.users.filter((u) => u.character.roleType === RoleType.TARGET || u.character.roleType === RoleType.BODYGUARD).map((u) => u.id),
+      winType: WinType.TARGET_AND_BODYGUARD_WIN,
+    } satisfies MessageProps<S2CGameEndNotification>);
 
-      rooms.removeRoom(roomId);
-      return;
-    }
+    rooms.removeRoom(roomId);
+    return;
+  }
 
-    if (isTargetAllDead(room)) {
-      room.broadcast(PACKET_TYPE.GAME_END_NOTIFICATION, {
-        winners: room.users.filter((u) => u.character.roleType === RoleType.HITMAN).map((u) => u.id),
-        winType: WinType.HITMAN_WIN,
-      } satisfies MessageProps<S2CGameEndNotification>);
+  if (isTargetAllDead(room)) {
+    room.broadcast(PACKET_TYPE.GAME_END_NOTIFICATION, {
+      winners: room.users.filter((u) => u.character.roleType === RoleType.HITMAN).map((u) => u.id),
+      winType: WinType.HITMAN_WIN,
+    } satisfies MessageProps<S2CGameEndNotification>);
 
-      rooms.removeRoom(roomId);
-      return;
-    }
+    rooms.removeRoom(roomId);
+    return;
+  }
 
-    if (isAllDeadExceptPsychopath(room)) {
-      room.broadcast(PACKET_TYPE.GAME_END_NOTIFICATION, {
-        winners: room.users.filter((u) => u.character.roleType === RoleType.PSYCHOPATH).map((u) => u.id),
-        winType: WinType.PSYCHOPATH_WIN,
-      } satisfies MessageProps<S2CGameEndNotification>);
+  if (isAllDeadExceptPsychopath(room)) {
+    room.broadcast(PACKET_TYPE.GAME_END_NOTIFICATION, {
+      winners: room.users.filter((u) => u.character.roleType === RoleType.PSYCHOPATH).map((u) => u.id),
+      winType: WinType.PSYCHOPATH_WIN,
+    } satisfies MessageProps<S2CGameEndNotification>);
 
-      rooms.removeRoom(roomId);
-      return;
-    }
-  };
+    rooms.removeRoom(roomId);
+    return;
+  }
 }
