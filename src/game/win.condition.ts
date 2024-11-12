957849880e0255b1c333e2093d4ca3189e9a48f4
BJS-kr <hahajeng1234@gmail.com>
2024-11-12 12:30:39 +0900

    feat: adjust win condition

--- src/game/win.condition.ts
+++ src/game/win.condition.ts
@@ -4,19 +4,25 @@ import { MessageProps } from '../protobuf/props';
 import { Room, Rooms } from '../rooms/types';
 import { log } from '../utils/logger';
 
-function isHitMenAllDead(room: Room) {
-  return room.users.filter((u) => u.character.roleType === RoleType.HITMAN).every((u) => u.character.isDead());
+function isAllDeadExceptTargetAndBodyguard(room: Room) {
+  return room.users
+    .filter((u) => u.character.roleType !== RoleType.TARGET && u.character.roleType !== RoleType.BODYGUARD)
+    .every((u) => u.character.isDead());
 }
 
-function isTargetAllDead(room: Room) {
-  return room.users.filter((u) => u.character.roleType === RoleType.TARGET).every((u) => u.character.isDead());
+function isTargetAllDeadAndHitManAlive(room: Room) {
+  return (
+    room.users.filter((u) => u.character.roleType === RoleType.TARGET).every((u) => u.character.isDead()) &&
+    room.users.filter((u) => u.character.roleType === RoleType.HITMAN).some((u) => !u.character.isDead())
+  );
 }
 
 function isAllDeadExceptPsychopath(room: Room) {
   return room.users.filter((u) => u.character.roleType !== RoleType.PSYCHOPATH).every((u) => u.character.isDead());
 }
+
 export function checkWinCondition(rooms: Rooms, room: Room, roomId: number) {
-  if (isHitMenAllDead(room)) {
+  if (isAllDeadExceptTargetAndBodyguard(room)) {
     room.broadcast(PACKET_TYPE.GAME_END_NOTIFICATION, {
       winners: room.users.filter((u) => u.character.roleType === RoleType.TARGET || u.character.roleType === RoleType.BODYGUARD).map((u) => u.id),
       winType: WinType.TARGET_AND_BODYGUARD_WIN,
@@ -26,7 +32,7 @@ export function checkWinCondition(rooms: Rooms, room: Room, roomId: number) {
     return;
   }
 
-  if (isTargetAllDead(room)) {
+  if (isTargetAllDeadAndHitManAlive(room)) {
     room.broadcast(PACKET_TYPE.GAME_END_NOTIFICATION, {
       winners: room.users.filter((u) => u.character.roleType === RoleType.HITMAN).map((u) => u.id),
       winType: WinType.HITMAN_WIN,
