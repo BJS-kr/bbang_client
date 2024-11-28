d323556c7602676e83461d1a0da5c359d238914c
BJS-kr <hahajeng1234@gmail.com>
2024-11-28 16:46:13 +0900

    feat: add no db

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,10 +2,15 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
 
-const poolConnection = mysql.createPool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+export let db: any;
+export const mockDb: any[] = [];
 
-export const db = drizzle(poolConnection, { schema, mode: 'default' });
+if (!process.env.NO_DB) {
+  const poolConnection = mysql.createPool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+
+  db = drizzle(poolConnection, { schema, mode: 'default' });
+}
--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -1,15 +1,19 @@
-import { v4 as uuidv4 } from 'uuid';
 import type { InsertedUser, Result, SelectedUser } from '../db/types';
-import { db } from '../db/db';
+import { db, mockDb } from '../db/db';
 import { $users } from '../db/schema';
 import { identity } from '../utils/ident';
 import { eq } from 'drizzle-orm';
 import CustomError from '../utils/customError';
 import { ERROR_CODES } from '../constants/error';
 
+console.log(db);
 export const createUser = async (email, password, nickname): Promise<Result<InsertedUser>> => {
   const user: InsertedUser = { email, password, nickname };
 
+  if (!db) {
+    mockDb.push(user);
+    return user;
+  }
   const result = await db
     .insert($users)
     .values(user)
@@ -19,6 +23,16 @@ export const createUser = async (email, password, nickname): Promise<Result<Inse
 };
 
 export const getUserByUserEmail = async (email: string): Promise<Result<SelectedUser>> => {
+  if (!db) {
+    const user = mockDb.find((user) => user.email === email);
+
+    if (!user) {
+      return new CustomError(ERROR_CODES.USER_NOT_FOUND, 'user not found');
+    }
+
+    return user;
+  }
+
   const user = await db.query.$users.findFirst({
     where: eq($users.email, email),
   });
